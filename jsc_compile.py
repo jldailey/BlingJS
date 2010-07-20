ba883720d129f04e93623fcb049b13f7d38f8d51
Jesse Dailey <jesse.dailey@gmail.com>
2010-07-20 12:54:03 -0400

    added a script to minify using google's closure compiler

---
+++ jsc_compile.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3.1
+import sys
+from http.client import HTTPConnection
+from urllib.parse import urlencode
+
+
+infile = sys.argv[1]
+outfile = sys.argv[2]
+
+with open(infile, 'r') as i:
+	data = i.read()
+	params = urlencode([
+		('compilation_level', 'ADVANCED_OPTIMIZATIONS'),
+		('output_info', 'compiled_code'),
+		('output_format', 'text'),
+		('js_code', data),
+	])
+	headers = {'Content-type': 'application/x-www-form-urlencoded'}
+	conn = HTTPConnection('closure-compiler.appspot.com')
+	conn.request('POST', '/compile', params, headers)
+	resp = conn.getresponse()
+	print("Status: %d" % resp.status)
+	print(resp.getheaders())
+	data = resp.read()
+	with open(outfile, "wb") as o:
+		o.write(data)
+	conn.close()
+
+

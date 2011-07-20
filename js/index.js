8123e6d60461ca57d351c5431c74208bd93c5ba5
Jesse Dailey <jesse.dailey@gmail.com>
2011-07-19 21:35:53 -0400

    WIP on creating a site branch

--- js/index.js
+++ js/index.js
@@ -12,12 +12,11 @@ try {
 			p.text(text.replace(num_re, "$1/$2"))
 		})
 	})
+	// replace tabs with spaces in <pre>'s
+	// make all <pre>'s pretty-printable
 	.ready(function () {
 		var initial_tab = /(^|\n)\t/g,
 			all_tabs = /\t/g
-
-		// replace tabs with spaces in <pre>'s
-		// make all <pre>'s pretty-printable
 		$("pre").zipzapmap('innerHTML', function(html) {
 			return Function.PrettyPrint($.HTML.escape(html)
 				.replace(all_tabs, "  "));
@@ -25,10 +24,6 @@ try {
 		// clean up textarea's also
 		$("textarea").zipzapmap('value', function (value) {
 			return value.replace(initial_tab,'$1').replace(all_tabs, "  ")
-		})
-		// all documentation links are made active
-		var a = $("a[href^='#doc:']").zipzap('href', function(href) {
-			return 'html/doc.html#' + href.split(/#doc:/)[1]
 		})
 	})
 } catch ( err ) {

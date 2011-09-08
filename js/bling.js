d9a82bccb3decc79869aecb31774535e717ebf4a
Jesse Dailey <jesse.dailey@gmail.com>
2011-09-07 21:13:33 -0400

    build annotation

--- js/bling.js
+++ js/bling.js
@@ -1488,6 +1488,25 @@
     $.plugin(function() {
       return {
         name: 'Maths',
+        $: {
+          range: function(start, end, step) {
+            var a, i, _results;
+            if (step == null) {
+              step = 1;
+            }
+            i = start;
+            if (step === 0 || (step > 0 && start > end) || (step < 0 && start < end)) {
+              return [];
+            }
+            _results = [];
+            while ((start > end && i > end) || (start < end && i < end)) {
+              a = i;
+              i += step;
+              _results.push(a);
+            }
+            return _results;
+          }
+        },
         floats: function() {
           return this.map(parseFloat);
         },
@@ -2385,35 +2404,53 @@
       };
     });
     $.plugin(function() {
+      var create, lazy_load;
+      create = function(elementName, props) {
+        return Object.Extend(document.createElement(elementName), props);
+      };
+      lazy_load = function(elementName, props) {
+        var n;
+        n = create(elementName, Object.Extend(props, {
+          onload: function() {
+            if (provides !== null) {
+              return $.publish(provides);
+            }
+          }
+        }));
+        $("head").delay(10, function() {
+          if (depends !== null) {
+            return $.subscribe(depends, __bind(function() {
+              return this.append(n);
+            }, this));
+          } else {
+            return this.append(n);
+          }
+        });
+        n = $(n);
+        return Object.Extend(n, {
+          depends: function(tag) {
+            var depends;
+            depends = elementName + "-" + tag;
+            return n;
+          },
+          provides: function(tag) {
+            var provides;
+            provides = elementName + "-" + tag;
+            return n;
+          }
+        });
+      };
       return {
         name: "LazyLoader",
         $: {
           script: function(src) {
-            var depends, provides, s;
-            provides = depends = null;
-            s = document.createElement("script");
-            s.src = src;
-            s.onload = function() {
-              if (provides !== null) {
-                return $.publish(provides);
-              }
-            };
-            $("head").delay(10, function() {
-              if (depends !== null) {
-                return $.subscribe(depends, __bind(function() {
-                  return this.append(s);
-                }, this));
-              } else {
-                return this.append(s);
-              }
+            return lazy_load("script", {
+              src: src
             });
-            return Object.Extend($(s), {
-              depends: function(tag) {
-                return depends = "onload-" + tag;
-              },
-              provides: function(tag) {
-                return provides = "onload-" + tag;
-              }
+          },
+          style: function(src) {
+            return lazy_load("style", {
+              href: src
             });
           }
         }

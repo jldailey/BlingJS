4dd483fb09c18c25144d7ec35463dbd92acdbe2d
Jesse Dailey <jesse.dailey@gmail.com>
2013-02-15 11:06:41 -0500

    make site

--- js/bling.js
+++ js/bling.js
@@ -145,6 +145,58 @@
   $ = Bling;
 
   $.plugin({
+    provides: "EventEmitter",
+    depends: "type,hook"
+  }, function() {
+    return {
+      $: {
+        EventEmitter: $.hook("bling-init").append(function(obj) {
+          var list, listeners;
+          if (obj == null) {
+            obj = Object.create(null);
+          }
+          listeners = {};
+          list = function(e) {
+            return listeners[e] || (listeners[e] = []);
+          };
+          return $.inherit({
+            emit: function() {
+              var a, e, f, _i, _len, _ref;
+              e = arguments[0], a = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
+              _ref = list(e);
+              for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+                f = _ref[_i];
+                f.apply(this, a);
+              }
+              return this;
+            },
+            addListener: function(e, h) {
+              list(e).push(h);
+              return this.emit('newListener', e, h);
+            },
+            on: function(e, h) {
+              return this.addListener(e, h);
+            },
+            removeListener: function(e, h) {
+              var i;
+              if ((i = list(e).indexOf(h)) > -1) {
+                return list(e).splice(i, 1);
+              }
+            },
+            removeAllListeners: function(e) {
+              return listeners[e] = [];
+            },
+            setMaxListeners: function(n) {},
+            listeners: function(e) {
+              return list(e).slice(0);
+            }
+          }, obj);
+        })
+      }
+    };
+  });
+
+  $.plugin({
     provides: "cartesian"
   }, function() {
     return {
@@ -351,13 +403,16 @@
         return this;
       },
       map: function(f) {
-        var b, t, _i, _len;
-        b = $();
-        for (_i = 0, _len = this.length; _i < _len; _i++) {
-          t = this[_i];
-          b.push(f.call(t, t));
-        }
-        return b;
+        var t;
+        return $((function() {
+          var _i, _len, _results;
+          _results = [];
+          for (_i = 0, _len = this.length; _i < _len; _i++) {
+            t = this[_i];
+            _results.push(f.call(t, t));
+          }
+          return _results;
+        }).call(this));
       },
       filterMap: function(f) {
         var b, t, v, _i, _len;
@@ -1810,58 +1865,6 @@
       };
     });
   }
-
-  $.plugin({
-    provides: "EventEmitter",
-    depends: "type,hook"
-  }, function() {
-    return {
-      $: {
-        EventEmitter: $.hook("bling-init").append(function(obj) {
-          var list, listeners;
-          if (obj == null) {
-            obj = Object.create(null);
-          }
-          listeners = {};
-          list = function(e) {
-            return listeners[e] || (listeners[e] = []);
-          };
-          return $.inherit({
-            emit: function() {
-              var a, e, f, _i, _len, _ref;
-              e = arguments[0], a = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
-              _ref = list(e);
-              for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-                f = _ref[_i];
-                f.apply(this, a);
-              }
-              return this;
-            },
-            addListener: function(e, h) {
-              list(e).push(h);
-              return this.emit('newListener', e, h);
-            },
-            on: function(e, h) {
-              return this.addListener(e, h);
-            },
-            removeListener: function(e, h) {
-              var i;
-              if ((i = list(e).indexOf(h)) > -1) {
-                return list(e).splice(i, 1);
-              }
-            },
-            removeAllListeners: function(e) {
-              return listeners[e] = [];
-            },
-            setMaxListeners: function(n) {},
-            listeners: function(e) {
-              return list(e).slice(0);
-            }
-          }, obj);
-        })
-      }
-    };
-  });
 
   $.plugin({
     depends: "dom,function,core",

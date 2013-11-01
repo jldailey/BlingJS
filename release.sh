68ad06f5af688e1546e97b14199b724d9f96ea1a
Jesse Dailey <jesse.dailey@gmail.com>
2013-11-01 18:07:53 -0400

    sed inconsistency?

--- release.sh
+++ release.sh
@@ -6,7 +6,7 @@ fi
 
 echo Making new release: $2 from current release: $1... && \
 	echo Patching package.json && \
-	sed -i .bak -e "s/$1/$2/" package.json && \
+	sed -ibak -e "s/$1/$2/" package.json && \
 	rm package.json.bak && \
 	echo Committing package.json && \
 	git commit package.json -m "v$2" &> /dev/null && \

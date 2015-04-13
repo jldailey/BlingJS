26d9b2df64b2e8595b81c8c7ae72f05858612381
Jesse Dailey <jesse.dailey@gmail.com>
2015-04-13 15:09:30 -0400

    release.sh: cleanup the output

--- release.sh
+++ release.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 
-if [ -z "$2" ]; then
+if [ -z "$1" ]; then
 	exit 0
 fi
 
 OLD=`cat VERSION`
 NEW=$1
 echo Making new release: $NEW from current release: $OLD... && \
-	echo Patching package.json && \
+	echo Patching package.json... && \
 	sed -i.bak -e "s/$OLD/$NEW/" package.json && \
 	rm package.json.bak && \
+	echo Writing VERSION file... && \
 	echo $NEW > VERSION &&
 	echo Committing package.json && \
 	git commit package.json VERSION -m "v$NEW" &> /dev/null && \
-	echo "Buiding site branch..." && \
+	echo Buiding site branch... && \
 	make site && \
-	echo "Pushing to github..." && \
+	echo Pushing to github... && \
 	git push && \
-	echo "Pushing site to blingjs.com" && \
+	echo Pushing site to blingjs.com... && \
 	ssh blingjs.com "cd /var/www/blingjs.com; and git pull" && \
-	echo Publishing to npm && \
+	echo Publishing to npm... && \
 	npm publish
